8eef60d951d72156ae5cc3682b1a8662769171e7
victorcid <victor.cid@code.seat>
2023-07-15 13:45:04 +0300

    Exercise 1 Flask

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
-# db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    """
+    id: db columns -> db.Integer, primary key
+    name: db columns -> db.String, unique
+    """
+
+
+@app.before_request_funcs
+def create_tables():
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return "Hello SDA students!"
+    """Functionalities for the users
 
-
-@app.route("/explore")
-def explore():
-    return "Hello explore!"
-
-
-@app.route("/notifications")
-def notifications():
-    return "Hello notifications!"
-
-
-@app.route("/messages")
-def messages():
-    return "Hello messages!"
+    1. Add a new user
+        functions to use
+            User
+            db.session.add
+            db.session.commit
+    2. Get all users
+            User.query.all
+            produce the output -> create a output variable to return
+    """
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
